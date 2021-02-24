c6a39b7411dbcb5922d0b6a1d6d7afadeae6bd41
wingstone <1812467051@qq.com>
2021-02-25 00:01:56 +0800

    add tool

---
+++ Assets/Scripts/KeyFrameInter/KeyFrameInter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyFrameInter
+{
+    public float Evaluate(float t, Keyframe keyframe0, Keyframe keyframe1)
+    {
+        float dt = keyframe1.time - keyframe0.time;
+
+        float m0 = keyframe0.outTangent * dt;
+        float m1 = keyframe1.inTangent * dt;
+
+        float t2 = t * t;
+        float t3 = t2 * t;
+
+        float a = 2 * t3 - 3 * t2 + 1;
+        float b = t3 - 2 * t2 + t;
+        float c = t3 - t2;
+        float d = -2 * t3 + 3 * t2;
+
+        return a * keyframe0.value + b * m0 + c * m1 + d * keyframe1.value;
+    }
+}
