b9fe109a3d7002635c927dfa185935c0a5b391f1
wingstone <1812467051@qq.com>
2021-05-05 18:26:06 +0800

    add screen

--- Assets/ScreenPainter/Res/Wave.shader
+++ Assets/ScreenPainter/Res/Wave.shader
@@ -6,6 +6,7 @@
         _Radius("redius", float) = 0.01
         _Point("point", Vector) = (0.5, 0.5, 0,0)
         _Inten("inten", float) = 5
+        _Fade("fade", Range(0, 1)) = 0.94
     }
     SubShader
     {
@@ -35,6 +36,7 @@
         float2 _Point;
         float _Radius;
         float _Inten;
+        float _Fade;
 
         v2f vert (appdata v)
         {
@@ -55,14 +57,23 @@
                 float4 left = tex2D(_MainTex, i.uv + float2(-_MainTex_TexelSize.x, 0));
                 float4 right = tex2D(_MainTex, i.uv + float2(_MainTex_TexelSize.x, 0));
                 float4 center = tex2D(_MainTex, i.uv);
-                float4 col = center;
 
-                col = (up + bottom + right + left)*0.5 - center;
+                float4 col = 0;
 
-                col *= 15.0/32;
-                col += _Inten*(1 - smoothstep(0, _Radius, distance(i.uv, _Point)));
+                // smooth
+                col.x = (up.x + bottom.x + right.x + left.x)*0.5 - center.y;
 
-                return saturate(col);
+                // fade
+                col.x *= _Fade;
+
+                // Mouse influence
+				float mousePhase = clamp( length( i.uv - float2( _Point.x, _Point.y ) ) * UNITY_PI / _Radius, 0.0, UNITY_PI );
+				col.x += (cos( mousePhase ) + 1.0)*_Inten;
+
+                // col.x += _Inten*(1 - smoothstep(0, _Radius, distance(i.uv, _Point)));
+
+                col.y = center.x;
+                return (col);
             }
             ENDCG
         }
@@ -72,8 +83,26 @@
             CGPROGRAM
             float4 frag (v2f i) : SV_Target
             {
+                float4 up = tex2D(_MainTex, i.uv + float2(0, _MainTex_TexelSize.y));
+                float4 bottom = tex2D(_MainTex, i.uv + float2(0, -_MainTex_TexelSize.y));
+                float4 left = tex2D(_MainTex, i.uv + float2(-_MainTex_TexelSize.x, 0));
+                float4 right = tex2D(_MainTex, i.uv + float2(_MainTex_TexelSize.x, 0));
+                float4 center = tex2D(_MainTex, i.uv);
+
+                float4 col = 0.25 * (up + bottom + left + right + center);
+                return col;
+            }
+            ENDCG
+        }
+
+        Pass    // 2
+        {
+            CGPROGRAM
+            float4 frag (v2f i) : SV_Target
+            {
                 float4 wave = tex2D(_WaveTex, i.uv);
-                float4 col = tex2D(_MainTex, i.uv + wave.xx);
+                // float4 col = tex2D(_MainTex, i.uv+_MainTex_TexelSize.xy*wave.x);
+                float4 col = wave.x;
                 return col;
             }
             ENDCG
--- Assets/ScreenPainter/Runtime/ScreenPainter.cs
+++ Assets/ScreenPainter/Runtime/ScreenPainter.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// ref: https://threejs.org/examples/#webgl_gpgpu_water
+// ref: https://github.com/mrdoob/three.js/blob/dev/examples/webgl_gpgpu_water.html
 [RequireComponent(typeof(Camera))]
 public class ScreenPainter : MonoBehaviour
 {
+    [Range(0.001f, 0.1f)]
     public float radius = 0.01f;
-    public float inten = 5f;
+    [Range(0.1f, 5f)]
+    public float inten = 1f;
+    [Range(0.9f, 1f)]
+    public float fade = 0.98f;
 
     RenderTexture rt;
     Material mat;
@@ -19,7 +25,7 @@ public class ScreenPainter : MonoBehaviour
         width = camera.pixelWidth;
         height = camera.pixelHeight;
 
-        rt = RenderTexture.GetTemporary(width, height, 0, RenderTextureFormat.RFloat);
+        rt = RenderTexture.GetTemporary(width, height, 0, RenderTextureFormat.RGB111110Float);
         Shader shader = Shader.Find("Wave");
         mat = new Material(shader);
         mat.hideFlags = HideFlags.HideAndDontSave;
@@ -30,8 +36,10 @@ public class ScreenPainter : MonoBehaviour
         RenderTexture.ReleaseTemporary(rt);
     }
 
-    void Update()
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
     {
+        RenderTexture tmp = RenderTexture.GetTemporary(width, height, 0, RenderTextureFormat.RGB111110Float);
+
         if (Input.GetMouseButton(0))    // 0: left 1: right 2: middle
         {
             Vector3 mousePos = Input.mousePosition;
@@ -41,22 +49,29 @@ public class ScreenPainter : MonoBehaviour
             mat.SetVector("_Point", mousePos);
             mat.SetFloat("_Radius", radius);
             mat.SetFloat("_Inten", inten);
+            mat.SetFloat("_Fade", fade);
         }
         else
         {
             mat.SetFloat("_Inten", 0);
         }
-    }
-
-    private void OnRenderImage(RenderTexture src, RenderTexture dest)
-    {
-        RenderTexture tmp = RenderTexture.GetTemporary(width, height, 0, RenderTextureFormat.RFloat);
 
         Graphics.Blit(rt, tmp, mat, 0);
-        mat.SetFloat("_Inten", 0);
-        Graphics.Blit(tmp, rt, mat, 0);
+
+        // smooth key down
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Graphics.Blit(tmp, rt, mat, 1);
+                Graphics.Blit(rt, tmp, mat, 1);
+            }
+        }
+
+        Graphics.Blit(tmp, rt);
+
         Shader.SetGlobalTexture("_WaveTex", rt);
-        Graphics.Blit(src, dest, mat, 1);
+        Graphics.Blit(src, dest, mat, 2);
 
         RenderTexture.ReleaseTemporary(tmp);
     }
